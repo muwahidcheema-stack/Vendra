1ba6f74573af938f5cad90d3addbba4774e75746
muwahidcheema-stack <muahidcheema880@gmail.com>
2026-09-17 00:28:44 +0500

    Categories, Products & Models

--- backend/src/api/v1/endpoints/products.py
+++ backend/src/api/v1/endpoints/products.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter,Depends,HTTPException,status, Query
 from decimal import Decimal
 import math
-from sqlalchemy import select, asc,desc,or_
+# from sqlalchemy import select, asc,desc,or_, 
+from sqlalchemy import select, func, or_, desc, asc
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
@@ -14,11 +15,77 @@ router = APIRouter(
     tags=["Products"]
 )
 
-# @router.get("", response_model=PaginationProductResponse)
-# async def list_products(
-#     search: str | None = Query(None, description = "Search across name and description"),
-#     category_id: id | None = Query(None, description="Filter by category"),
-#     is_active: bool | None = Query(None, description="Filter featured products"),
-#     db: AsyncSession = Depends(get_db)
-# ):
-#     return
+@router.get("", response_model=PaginationProductResponse)
+async def list_products(
+    search: str | None = Query(None, description = "Search across name and description"),
+    category_id: id | None = Query(None, description="Filter by category"),
+    min_price: Decimal | None = Query(None, ge=0, description="Minimum price of products"),
+    max_price: Decimal | None = Query(None, ge=0, description="Maximum price of products"),
+    sort: str = Query("newest", regex="^(newest|price_asc|price_desc)$"),
+    page: int = Query(1, ge=0, description="Total Number of pages"),
+    page_size: int = Query(12, ge=0, le=100, description="Products per Page"),
+    is_active: bool | None = Query(None, description="Filter featured products"),
+    db: AsyncSession = Depends(get_db)
+):
+    filters = [Product.is_active.is_(True)]
+
+    if search and search.strip():
+        search_pattern = f"%{search.strip()}%"
+        filters.append(
+            or_(
+                Product.name.ilike(search_pattern),
+                Product.description.ilike(search_pattern),
+            )
+        )
+    if category_id is not None:
+        filters.append(Product.category_id == category_id)
+    if min_price is not None:
+        filters.append(Product.price >= min_price)
+    if max_price is not None:
+        filters.append(Product.price <= max_price)
+    if is_active is not None:
+        filters.append(Product.is_active.is_(is_active))
+
+    # Total filters
+    count_query = select(func.count(Product.id)).where(*filters)
+    total_products = (await db.execute(count_query)).scalar() or 0
+
+    # Base Query
+    query = select(Product).where(*filters)
+
+    # Sorting
+    if sort == "price_asc":
+        query = query.order_by(asc(Product.price))
+    elif sort == "price_desc":
+        query = query.order_by(desc(Product.price))
+    else:
+        query = query.order_by(desc(Product.created_at))
+
+    # Offset and Limits
+    offset_value = (page -1) * page_size
+    query = query.offset(offset_value).limit(page_size)
+
+    result = await db.execute(query)
+    products = result.scalars().all()
+
+    total_pages = math.ceil(total_products / page_size) if total_products > 0 else 0
+
+    return PaginationProductResponse(
+        items= [ProductListResponse.model_validate(p) for p in products],
+        total=total_products,
+        page=page,
+        page_size=page_size,
+        pages=total_pages
+    )
+
+@router.get("/{id}", response_model=ProductDetailResponse)
+async def get_product_details(id: int, db: AsyncSession = Depends(get_db)):
+    stmt = (
+        select(Product)
+        .where(Product.id == id, Product.is_active.is_(True))
+        .options(
+            selectinload(Product.category),
+            selectinload(Product)
+        )
+    )
+
--- backend/src/models/models.py
+++ backend/src/models/models.py
@@ -1,6 +1,7 @@
 import enum 
-from datetime import datetime
-from sqlalchemy import String, Integer, Boolean, Text, Enum, ForeignKey, DateTime, Numeric, CheckConstraint
+from datetime import datetime, timezone
+from sqlalchemy import String, Integer, Boolean, Text, Enum, ForeignKey, DateTime, Numeric, CheckConstraint, UniqueConstraint
+from decimal import Decimal
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..core.database import Base
 
@@ -8,7 +9,7 @@ class UserRole(str, enum.Enum):
     CUSTOMER = "customer"
     ADMIN = "admin"
 
-class OrderStaus(str, enum.Enum):
+class OrderStatus(str, enum.Enum):
     PENDING = "pending"
     CONFIRMED = "confirmed"
     PROCESSING = "processing"
@@ -23,18 +24,23 @@ class User(Base):
     email: Mapped[str] = mapped_column(String, nullable=False, index=True, unique=True)
     full_name: Mapped[str] = mapped_column(String, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
-    phone: Mapped[str] = mapped_column(String, nullable=True)
-    avatar_url: Mapped[str] = mapped_column(String, nullable=True)
+    phone: Mapped[str | None] = mapped_column(String, nullable=True)
+    avatar_url: Mapped[str | None] = mapped_column(String, nullable=True)
     role: Mapped[UserRole]= mapped_column(Enum(UserRole), default=UserRole.CUSTOMER)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
 
+    orders: Mapped[list[Order]] = relationship(back_populates="user")
+    cart_items: Mapped[list["CartItem"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+    wishlist_items: Mapped[list["WishList"]] = relationship(back_populates="user" , cascade="all, delete-orphan")
+    reviews: Mapped[list["Review"]] = relationship(back_populates="user", cascade="all, delete-orphan",)
+    
 class Category(Base):
     __tablename__ = "categories"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
-    slug: Mapped[str] = mapped_column(String, unique=True, index=True)
-    image_url: Mapped[str] = mapped_column(String, nullable=True)
+    slug: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    image_url: Mapped[str | None] = mapped_column(String, nullable=True)
 
     products: Mapped[list["Product"]] = relationship(back_populates="category")
 
@@ -42,15 +48,22 @@ class Product(Base):
     __tablename__ = "products"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    name: Mapped[int] = mapped_column(String, index=True, nullable=False)
-    price: Mapped[float] = mapped_column(Numeric(10,2), nullable=False)
-    description: Mapped[str] = mapped_column(Text, nullable=True)
-    stock: Mapped[int] = mapped_column(Integer, default=0)
-    image_url: Mapped[str] = mapped_column(String, nullable=True)
+    name: Mapped[str] = mapped_column(String, index=True, nullable=False)
+    price: Mapped[Decimal] = mapped_column(Numeric(10,2), nullable=False)
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+    stock: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    image_url: Mapped[str | None] = mapped_column(String, nullable=True)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False, index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
 
     category: Mapped["Category"] = relationship(back_populates="products")
+    reviews: Mapped[list["Review"]] = relationship(back_populates="product", cascade="all, delete-orphan")
+
+    __table_args__ = (
+        CheckConstraint("stock >= 0", name="check_product_stock_nonnegative"),
+    )
+
 
 class CartItem(Base):
     __tablename__ = "cart_items"
@@ -59,33 +72,42 @@ class CartItem(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True)
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id", ondelete="CASCADE"))
     quantity: Mapped[int] = mapped_column(Integer, default=1)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
 
     user: Mapped["User"] = relationship()
-    products: Mapped["Product"] = relationship()
+    product: Mapped["Product"] = relationship()
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "product_id", name="uq_cart_user_product",),
+        CheckConstraint("quantity > 0" , name="check_cart_quantity_positive")
+    )
 
 class WishList(Base):
-    __tablename__ = "wishlist"
+    __tablename__ = "wishlists"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, index=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True)
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id", ondelete="CASCADE"))
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
-    
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+
     user: Mapped["User"] = relationship()
-    products: Mapped["Product"] = relationship()
+    product: Mapped["Product"] = relationship()
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "product_id", name="uq_wishlist_user_product",)
+    )
 
 class Order(Base):
     __tablename__ = "orders"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, index=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
-    shipping_address: Mapped[String] = mapped_column(Text, nullable=False)
-    total_amount: Mapped[float] = mapped_column(Numeric(10,2), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True, nullable=False)
+    shipping_address: Mapped[str] = mapped_column(Text, nullable=False)
+    total_amount: Mapped[Decimal] = mapped_column(Numeric(10,2), nullable=False)
     payment_method: Mapped[str] = mapped_column(String, default="COD")
-    status: Mapped[OrderStaus] = mapped_column(Enum(OrderStaus), default=OrderStaus.PENDING)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
-        
+    status: Mapped[OrderStatus] = mapped_column(Enum(OrderStatus), default=OrderStatus.PENDING)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+
     user: Mapped["User"] = relationship()
     items: Mapped[list["OrderItem"]] = relationship(back_populates="order", cascade="all, delete-orphan")
 
@@ -93,14 +115,18 @@ class OrderItem(Base):
     __tablename__ = "order_items"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, index=True)
-    product_id: Mapped[int] = mapped_column(ForeignKey("products.id", ondelete="CASCADE"))
+    product_id: Mapped[int | None] = mapped_column(ForeignKey("products.id", ondelete="SET NULL"))
     order_id: Mapped[int] = mapped_column(ForeignKey("orders.id", ondelete="CASCADE"), index=True)
-    quantity: Mapped[int] = mapped_column(Integer, default=1)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
-    price: Mapped[float] = mapped_column(Numeric(10,2), nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, default=1, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)    
+    price: Mapped[Decimal] = mapped_column(Numeric(10,2), nullable=False)
 
-    products: Mapped["Product"] = relationship()
+    product: Mapped["Product"] = relationship()
     order: Mapped["Order"] = relationship(back_populates = "items")
+
+    __table_args__ = (
+        CheckConstraint("quantity > 0", name="check_order_item_quantity_positive",)
+    )
 
 class Review(Base):
     __tablename__ = "reviews"
@@ -110,9 +136,14 @@ class Review(Base):
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id", ondelete="CASCADE"))
     order_id: Mapped[int] = mapped_column(ForeignKey("orders.id", ondelete="CASCADE"), index=True)
     rating: Mapped[int] = mapped_column(Integer, nullable=False)
-    comment: Mapped[str] = mapped_column(Text,nullable=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    comment: Mapped[str | None] = mapped_column(Text,nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+
+    product: Mapped["Product"] = relationship(back_populates="reviews")
+    user: Mapped["User"] = relationship()
+    order: Mapped["Order"] = relationship()
 
     __table_args__ = (
         CheckConstraint("rating >= 1 AND rating <= 5", name="check_rating_range"),
+        UniqueConstraint("user_id", "product_id", "order_id", name="uq_review_user_product_order")
     )
